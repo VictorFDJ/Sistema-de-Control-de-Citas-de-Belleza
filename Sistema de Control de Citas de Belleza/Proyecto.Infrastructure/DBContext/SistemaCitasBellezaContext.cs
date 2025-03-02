327441ca38b4912803d0d63ad979c4317c4bc6a2
Victor Fernandez <147003648+VictorFDJ@users.noreply.github.com>
2025-03-02 12:20:31 -0400

    Cambie la cadena de conexión
    he cambiado la cadena de conexion al archivo appSenttings en el proyecto.web

--- Sistema de Control de Citas de Belleza/Proyecto.Infrastructure/DBContext/SistemaCitasBellezaContext.cs
+++ Sistema de Control de Citas de Belleza/Proyecto.Infrastructure/DBContext/SistemaCitasBellezaContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using proyecto.Domain.Models;
 
 namespace Sistema_de_Control_de_Citas_de_Belleza.Infrastructure.DBContext;
 
@@ -29,9 +30,8 @@ public partial class SistemaCitasBellezaContext : DbContext
 
     public virtual DbSet<Usuario> Usuarios { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=VICTORFDJ;Database=SistemaCitasBelleza;Integrated Security=True;TrustServerCertificate=True");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
